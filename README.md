ee5d6d4b6f26aa819f654417d68bbcb17eab4b14
NameSpace <sieon2779@gmail.com>
2025-08-12 00:35:49 +0900

    Improved code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 3D Brownian motion simulation
+
 파이썬으로 만든 3D 브라운 운동 시뮬레이션
+
+![animation](https://github.com/Name-hw/3D-Brownian-motion-simulation/blob/main/animation.gif)
--- app.py
+++ app.py
@@ -10,15 +10,32 @@ particles = []
 
 
 def init():
+    init_ax()
+    init_particles()
+
+    return ax.collections
+
+
+def init_ax():
     ax.set_title("3D Brownian motion simulation")
     ax.set(xlim3d=[-10, 10], xlabel="X-axis")
     ax.set(ylim3d=[-10, 10], ylabel="Y-axis")
     ax.set(zlim3d=[-10, 10], zlabel="Z-axis")
 
 
+def init_particles():
+    for _ in range(PARTICLE_COUNT):
+        position = np.array([0.0, 0.0, 0.0])
+        velocity = np.array([0.0, 0.0, 0.0])
+        particle = Particle(position, velocity)
+        ax.scatter(particle.position[0], particle.position[1], particle.position[2])
+
+        particles.append(particle)
+
+
 def update(frame):
     plt.cla()
-    init()
+    init_ax()
 
     for particle in particles:
         particle.move()
@@ -27,17 +44,7 @@ def update(frame):
     return ax.collections
 
 
-init()
-
-for _ in range(PARTICLE_COUNT):
-    position = np.array([0.0, 0.0, 0.0])
-    velocity = np.random.normal(0, 1, 3)
-    particle = Particle(position, velocity)
-    ax.scatter(particle.position[0], particle.position[1], particle.position[2])
-
-    particles.append(particle)
-
-anim = FuncAnimation(fig, update, frames=200, interval=50, blit=False)
+anim = FuncAnimation(fig, update, frames=200, init_func=init, interval=50, blit=False)
 # plt.show()
 
 anim.save("animation.gif")
